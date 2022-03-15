fa7d65c831d66f176e120fda0885de52fbbfd41a
Srivastava <yashsrivastava@deloitte.com>
2022-03-15 18:23:47 +0530

    project initialized

---
+++ index.js
@@ -0,0 +1,22 @@
+// This will our main page where we gonna work on only two funtion...
+
+
+
+// TODO 1: Create a function which will encode the String
+
+module.exports.encodePassword = (password) =>{
+    // create a functionality that will convert this password into hashed/secured password 
+}
+
+
+
+
+
+// TODO 2 : Create a function which will decode the string
+
+
+
+module.exports.decodePassword = (hashedpassword)=>{
+    // create a functionality that will decode this password into original password 
+}; 
+
